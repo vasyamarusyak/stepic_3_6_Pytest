c5f10cb2c2b3d2658663c750939d52a37624a6b1
Your Name <>
2019-12-16 01:36:24 +0200

    Add files: conftest.py and test_items.py

--- conftest.py
+++ conftest.py
@@ -2,8 +2,6 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-
-
 def pytest_addoption(parser):
     parser.addoption('--browser_name',\
                      action='store',\
