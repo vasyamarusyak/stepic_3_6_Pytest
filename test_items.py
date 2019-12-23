6c791eb12d3be973997e37953e0cfbd7bce52d23
Your Name <>
2019-12-24 00:36:41 +0200

    Changed assert method  to right method

--- test_items.py
+++ test_items.py
@@ -6,13 +6,8 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_button_add_to_basket_exists(browser):
     browser.get(link)
     #time.sleep(30)
-    btn_add_to_basket = browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
-    assert btn_add_to_basket, 'Button "Add to basket" is not found'
-    
-    #assert btn_add_to_basket != None
-    #try:
-    #    browser.find_element_by_css_selector(".btn-add-to-basket")
-    #except NoSuchElementException:
-    #   pytest.fail('Button "Add to basket" is not found')
+    btn_add_to_basket = len(browser.find_elements(By.CLASS_NAME,("btn-add-to-basket")))
+    assert btn_add_to_basket > 0, '!!!btn_add_to_basket did not find!!!'
+
 
 
